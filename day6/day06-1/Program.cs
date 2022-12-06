bd0902d9d1afb4bd417b5a146f2d5f286f5d0bb0
Stephen Hickey <stephen.hickey@avalara.com>
2022-12-06 09:25:33 -0500

    feat: day 6

---
+++ day6/day06-1/Program.cs
@@ -0,0 +1,18 @@
+﻿foreach (var line in File.ReadLines(args[0]))
+{
+    var marker = 0;
+    while (line.Length - 4 - marker > 0)
+    {
+        var four = line.Substring(marker, 4);
+        ++marker;
+
+        if (four.Distinct().Count() == 4)
+        {
+            marker += 3;
+            break;
+        }
+    }
+
+
+    Console.WriteLine(marker);
+}
