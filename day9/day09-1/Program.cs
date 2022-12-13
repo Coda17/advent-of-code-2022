2f31eb5f4d605aadfef35525901d61479e644f6d
Stephen Hickey <stephen.hickey@avalara.com>
2022-12-13 02:01:37 -0500

    feat: day 9

---
+++ day9/day09-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using day09_1;
+
+var rope = new Rope();
+var tailVisits = new HashSet<(int, int)>();
+foreach (var line in File.ReadLines(args[0]))
+{
+    var direction = line[0];
+    var distance = int.Parse(line.Split(' ')[1]);
+
+    for (var i = 0; i < distance; ++i)
+    {
+        rope.MoveHead(direction);
+        tailVisits.Add((rope.TailX, rope.TailY));
+    }
+}
+
+Console.WriteLine(tailVisits.Count);
---
+++ day9/day09-1/Rope.cs
@@ -0,0 +1,51 @@
+﻿namespace day09_1;
+
+public class Rope
+{
+    public int HeadX { get; private set; }
+    public int HeadY { get; private set; }
+
+    public int TailX { get; private set; }
+    public int TailY { get; private set; }
+
+    public void MoveHead(char direction)
+    {
+        switch (direction)
+        {
+            case 'L':
+                HeadX--;
+                if (TailX - HeadX > 1)
+                {
+                    TailX--;
+                    TailY = HeadY != TailY ? HeadY : TailY;
+                }
+                break;
+            case 'R':
+                HeadX++;
+                if (HeadX - TailX > 1)
+                {
+                    TailX++;
+                    TailY = HeadY != TailY ? HeadY : TailY;
+                }
+                break;
+            case 'U':
+                HeadY++;
+                if (HeadY - TailY > 1)
+                {
+                    TailY++;
+                    TailX = HeadX != TailX ? HeadX : TailX;
+                }
+                break;
+            case 'D':
+                HeadY--;
+                if (TailY - HeadY > 1)
+                {
+                    TailY--;
+                    TailX = HeadX != TailX ? HeadX : TailX;
+                }
+                break;
+            default:
+                throw new ApplicationException("Shouldn't happen");
+        }
+    }
+}
