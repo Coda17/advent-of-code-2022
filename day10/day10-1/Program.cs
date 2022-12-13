74382349fc5a822eb3d526326d06decd07517605
Stephen Hickey <stephen.hickey@avalara.com>
2022-12-13 11:27:56 -0500

    feat: day 10

---
+++ day10/day10-1/Program.cs
@@ -0,0 +1,28 @@
+﻿var cycle = 1;
+var x = 1;
+var signalStrength = 0;
+foreach (var line in File.ReadLines(args[0]))
+{
+    var split = line.Split(' ');
+    var op = split[0];
+
+    if (op == "addx")
+    {
+        ++cycle;
+        if ((cycle - 20) % 40 == 0)
+        {
+            signalStrength += cycle * x;
+        }
+
+        var v = int.Parse(split[1]);
+        x += v;
+    }
+
+    ++cycle;
+    if ((cycle - 20) % 40 == 0)
+    {
+        signalStrength += cycle * x;
+    }
+}
+
+Console.WriteLine(signalStrength);
