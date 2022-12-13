ff6e0c7ea4c5c40dea3cda47f8f171c0aef51456
Stephen Hickey <stephen.hickey@avalara.com>
2022-12-13 15:06:24 -0500

    feat: day 11

---
+++ day11/day11-1/Monkey.cs
@@ -0,0 +1,16 @@
+﻿namespace day11_1;
+
+public class Monkey
+{
+    public LinkedList<long> Items { get; init; }
+    public Func<long, long> Operation { get; init; }
+    public Func<long, int> Test { get; init; }
+    public long Inspections { get; set; } = 0;
+
+    public Monkey(LinkedList<long> items, Func<long, long> operation, Func<long, int> test)
+    {
+        Items = items;
+        Operation = operation;
+        Test = test;
+    }
+}
---
+++ day11/day11-2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+using day11_2;
+
+var lines = File.ReadAllLines(args[0]);
+var monkeys = new List<Monkey>((lines.Length + 1)/ 7);
+
+var i = 0;
+while (i < lines.Length)
+{
+    var items = lines[i + 1].Replace("  Starting items: ", string.Empty).Split(", ").Select(long.Parse);
+    var operations = lines[i + 2].Replace("  Operation: new = ", string.Empty).Split(" ");
+
+    var old = Expression.Parameter(typeof(long), "old");
+    Expression left = operations[0] == "old" ? old : Expression.Constant(long.Parse(operations[0]), typeof(long));
+    Expression right = operations[2] == "old" ? old : Expression.Constant(long.Parse(operations[2]), typeof(long));
+
+    var operation = Expression
+        .Lambda<Func<long, long>>(operations[1] == "+" ? Expression.Add(left, right) : Expression.Multiply(left, right), old)
+        .Compile();
+
+    var modulo = long.Parse(lines[i + 3].Replace("  Test: divisible by ", string.Empty));
+    var trueMonkey = int.Parse(lines[i + 4].Replace("If true: throw to monkey ", string.Empty));
+    var falseMonkey = int.Parse(lines[i + 5].Replace("If false: throw to monkey ", string.Empty));
+
+    var monkey = new Monkey(new LinkedList<long>(items), operation, modulo, trueMonkey, falseMonkey);
+    monkeys.Add(monkey);
+
+    i += 7;
+}
+
+var factor = monkeys.Select(x => x.Modulo).Aggregate(1L, (l, l1) => l * l1);
+for (var round = 1; round < 10001; ++round)
+{
+    foreach (var monkey in monkeys)
+    {
+        while (monkey.Items.Any())
+        {
+            var item = monkey.Items.First();
+            monkey.Items.RemoveFirst();
+
+            var newItem = monkey.Operation(item) % factor;
+
+            var newMonkey = newItem % monkey.Modulo == 0 ? monkey.TrueMonkey : monkey.FalseMonkey;
+            monkeys[newMonkey].Items.AddLast(newItem);
+
+            ++monkey.Inspections;
+        }
+    }
+}
+
+var inspections = monkeys.OrderByDescending(x => x.Inspections).Select(x => x.Inspections).ToArray();
+var monkeyBusiness = inspections[0] * inspections[1];
+
+Console.WriteLine(monkeyBusiness);
