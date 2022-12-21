ba484c44556a85d01f2bb561dc9bcef057aee666
Steve Hickey <hickeyst@gmail.com>
2022-12-20 22:53:01 -0500

    feat: day 13

---
+++ day13/day13-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using day13_2;
+
+var lines = File.ReadAllLines(args[0]);
+const string decoderKeyStart = "[[2]]";
+const string decoderKeyEnd = "[[6]]";
+var packets = lines.Where(x => !string.IsNullOrEmpty(x)).Append(decoderKeyStart).Append(decoderKeyEnd).ToArray();
+var ordered = packets.OrderBy(x => x, new PacketComparer()).ToList();
+
+var start = ordered.IndexOf(decoderKeyStart) + 1;
+var end = ordered.IndexOf(decoderKeyEnd) + 1;
+
+Console.WriteLine($"Decoder key = {start} * {end} = {start * end}");
---
+++ day13/day13-2/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace day13_2;
+
+internal enum Result
+{
+    NotOrdered = 0,
+    Ordered = 1,
+    Continue = 2
+}
